52b80bfba2c17a6ae3a9ba6348f2eabad93c5df1
Alexey Sur <alexeysur@gmail.com>
2022-11-16 10:35:24 +0200

    playground file with solution

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Vortex_iOS_Lab
 Test task for free online training course from Vortex
+
+# Task:
+
+Make a functions that takes a string and makes it look “cool” by replacing some letters with another symbols
+For example we can replace “a” or “A” with @ and get “с@t” instead of “cat”
+Letters to replace: a - @; i - 1; s - $; o - 0; t - +
+(replace both capital and lowercase letters)
+
+example:
+
+func makeItCool(_ string: String) -> String { … }
+
+makeItCool(“today”) -> “+0d@y”
+
+makeItCool(“Swift is awesame”) -> “$wif+ 1$ @we$@me”
---
+++ Vortex_iOS_Lab.playground/Contents.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+//Task:
+//Make a functions that takes a string and makes it look “cool” by replacing some letters with another symbols
+//For example we can replace “a” or “A” with @ and get “с@t” instead of “cat”
+//Letters to replace: a - @; i - 1; s - $; o - 0; t - +
+//(replace both capital and lowercase letters)
+
+
+func makeItCool(_ string: String) -> String {
+    var result: String = ""
+    
+    for char in string {
+        let symbol = char.lowercased()
+        switch symbol {
+          case "a": result += "@"
+          case "i": result += "1"
+          case "s": result += "$"
+          case "o": result += "0"
+          case "t": result += "+"
+          default:
+            result += symbol
+        }
+    }
+    return result
+}
+
+print(makeItCool("today"))
+print(makeItCool(""))
+print(makeItCool("Swift is awesame"))
